6289399f1f4172698d20b7fd844d2a823832286b
Serouj Djabraian <96635449+Djabraianserouj@users.noreply.github.com>
2022-02-05 19:41:07 +0100

    Delete 1.Basic_Image_Analytics directory

--- 1.Basic_Image_Analytics/Layers/Base.py
+++
@@ -1,6 +0,0 @@
-class BaseLayer:
-    # weights = []
-
-    def __init__(self):
-        self.trainable = False #Trainable layers have parameters that are optimized during training
-                              # the non trainable layers remained fixed
